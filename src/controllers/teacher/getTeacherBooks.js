a1fd9eff4846d955977d20e7ca976906b0e347cf
alanjuice <alnjoze@gmail.com>
2024-03-16 11:42:41 +0530

    get entire book details

--- src/controllers/teacher/getTeacherBooks.js
+++ src/controllers/teacher/getTeacherBooks.js
@@ -4,13 +4,14 @@ const pool = require("../../database/pool");
 async function getTeacherBooks(req, res) {
   try {
     const teacherId = req.user.id;
-    const results = await pool.query("SELECT bid FROM issues WHERE tid = $1 and return_date is null", [
-      teacherId,
-    ]);
+    const results = await pool.query(
+      "select * from books where id in (SELECT bid FROM issues WHERE tid = $1 and return_date is null)",
+      [teacherId]
+    );
 
     const books = results.rows;
 
-    res.json(books);
+    res.status(200).json(books);
   } catch (error) {
     console.error("Error fetching teacher's books:", error);
     res.status(500).json({ error: "Internal Server Error" });
