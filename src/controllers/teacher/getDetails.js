c3c5be2198118d7e11f78d8f554bb5caa6473784
Alan <93069058+alanjuice@users.noreply.github.com>
2024-04-02 20:38:46 +0530

    add more details

--- src/controllers/teacher/getDetails.js
+++ src/controllers/teacher/getDetails.js
@@ -7,8 +7,17 @@ async function getDetails(req, res) {
       "select name,classname,email from teachers where id = $1",
       [req.user.id]
     );
-    console.log(result.rows);
-    res.status(200).json(result.rows[0]);
+    const countQuery = `
+  SELECT 
+    (SELECT COUNT(*) FROM students WHERE classname = $1) as student_count,
+    (SELECT COUNT(*) FROM issues WHERE tid = $2 AND return_date IS NULL AND sid IS NOT NULL) as allocated_books_count;
+`;
+    const count = await pool.query(countQuery, [
+      req.user.classname,
+      req.user.id,
+    ]);
+    const finalData = { ...count.rows[0], ...result.rows[0] };
+    res.status(200).json(finalData);
   } catch (error) {
     console.log("Error", error);
     res.status(404).json({ status: false, msg: "Internal Server Error" });
