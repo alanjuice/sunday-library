8109b3e1022e279667d410facce8a7e8bf16a189
Alan <93069058+alanjuice@users.noreply.github.com>
2024-04-16 19:30:27 +0530

    fix get categories

--- src/controllers/admin/getCategories.js
+++ src/controllers/admin/getCategories.js
@@ -3,11 +3,8 @@ const pool = require("../../database/pool");
 //gets all book category
 async function getCategories(req, res) {
   try {
-    const result = await pool.query("select distinct CATEGORY from books");
-    const categoryNames = result.rows.map(
-      (categoryObj) => categoryObj.category
-    );
-    res.status(200).json({ status: true, data: categoryNames });
+    const result = await pool.query("select id,name from book_cat");
+    res.status(200).json({ status: true, data: result.rows });
   } catch (error) {
     res.status(404).json({ status: false, msg: "Something went wrong" });
   }
